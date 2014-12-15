c5728db9df386cd58dbf1163e026afa81d523e6c
Suresh <sureshviprak@gmail.com>
2014-12-15 19:57:49 +0530

    Change to reload script

--- greasemonkey/reload.user.js
+++ greasemonkey/reload.user.js
@@ -2,12 +2,11 @@
 // @name        Reload
 // @namespace   com.viprak.local
 // @description Reload page after set interval
-// @include     http://192.168.0.1/userRpm/SystemStatisticRpm.htm
-// @include     http://192.168.0.1/userRpm/StatusRpm.htm
+// @include     http://192.168.0.1/userRpm/SystemStatisticRpm.htm?interval=*
 // @include     http://119.160.192.114/userportal/accGraph.do?actid=vbpatel2
 // @version     1
 // @grant       GM_registerMenuCommand
 // ==/UserScript== 
 window.setTimeout(function(){
 	document.forms[0].submit();
-}, 2 * 60 * 1000)
+}, 3 * 60 * 1000);
